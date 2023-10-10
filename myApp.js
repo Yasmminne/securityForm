1794e2940f4da5dae6815f383a2c42ca50e40895
YASMINE <y_habi@estin.dz>
2023-10-10 17:01:53 +0100

    frameguard

--- myApp.js
+++ myApp.js
@@ -3,7 +3,11 @@ const helmet=require('helmet');
 const app = express();
  
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action:'deny'}));
+app.use(
+  helmet.frameguard({
+    action: "deny",
+  })
+);
 
 
 
