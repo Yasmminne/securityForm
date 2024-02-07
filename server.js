50cd3d724d1a23e915ddc05b1de96fa0e03bd81f
YASMINE <y_habi@estin.dz>
2024-02-07 15:46:26 +0100

    bcrypt2

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
-
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare('passw0rd!', hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
