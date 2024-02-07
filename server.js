f727d8676f569b7506d7b6dc076b0b89c713b6d3
YASMINE <y_habi@estin.dz>
2024-02-07 17:20:32 +0100

    bcrypt3

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 bcrypt.hash('passw0rd!', 13, (err, hash) => {
     console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
