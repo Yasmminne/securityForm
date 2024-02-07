a58467af4dd87514ba91c284eea19b72d76fb0d3
YASMINE <y_habi@estin.dz>
2024-02-07 17:41:50 +0100

    bcrypt6

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
      console.log(res);
